86d85a56d7567be9ef6aab3dbbe86cc94da4380d
kumapo <kumapo@users.noreply.github.com>
2016-03-04 23:28:38 +0900

    Support Carthage

---
+++ CollectionViewWaterfallLayout/CollectionViewWaterfallLayout.h
@@ -0,0 +1,19 @@
+//
+//  CollectionViewWaterfallLayout.h
+//  CollectionViewWaterfallLayout
+//
+//  Created by kumapo on 2016/03/04.
+//  Copyright © 2016年 kumapo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionViewWaterfallLayout.
+FOUNDATION_EXPORT double CollectionViewWaterfallLayoutVersionNumber;
+
+//! Project version string for CollectionViewWaterfallLayout.
+FOUNDATION_EXPORT const unsigned char CollectionViewWaterfallLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionViewWaterfallLayout/PublicHeader.h>
+
+
